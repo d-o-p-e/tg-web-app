02efcf4f3fd2510c0e62c2410e13df15e6c373b6
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-05-23 16:27:46 +0900

    feat: baseurl변경

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    import.meta.env.MODE !== 'production' ? import.meta.env.VITE_PROD_BASE_URL : import.meta.env.VITE_DEV_BASE_URL,
+    import.meta.env.MODE === 'production' ? import.meta.env.VITE_PROD_BASE_URL : import.meta.env.VITE_DEV_BASE_URL,
   headers: {
     'Content-type': 'application/json',
   },
