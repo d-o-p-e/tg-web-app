8f10465912ddef8e30558a1031ca6f8606255dad
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-05-26 12:01:16 +0900

    Update comment.ts

--- src/apis/comment.ts
+++ src/apis/comment.ts
@@ -2,13 +2,13 @@ import { Comment } from '@/typings/comment';
 import api from './axios';
 
 export const getComments = (postId: number) => {
-  return api.get<Comment[]>(`comment/${postId}/comments`);
+  return api.get<Comment[]>(`comment/${postId}/comment`);
 };
 
 export const postComment = ({ postId, content }: { postId: number; content: string }) => {
-  return api.post(`comment/${postId}/comments`, { content: content });
+  return api.post(`comment/${postId}/comment`, { content: content });
 };
 
 export const deleteComment = ({ postId, commentId }: { postId: number; commentId: number }) => {
-  return api.delete(`comment/${postId}/comments/${commentId}`);
+  return api.delete(`comment/${postId}/comment/${commentId}`);
 };
